8e481b6e700e1f8ed476c098dd8e77d098e4b928
electrodude <electrodude512@gmail.com>
2020-10-01 16:33:03 -0400

    Add AEM_GC_* convenience macros

--- gc.h
+++ gc.h
@@ -40,10 +40,29 @@ void aem_gc_init(struct aem_gc_context *ctx);
 void aem_gc_dtor(struct aem_gc_context *ctx);
 
 void aem_gc_register(struct aem_gc_object *obj, const struct aem_gc_vtbl *vtbl, struct aem_gc_context *ctx);
+#define AEM_GC_REGISTER(_tp, _obj, _ctx) aem_gc_register(&(_obj)->gc, &_tp##_vtbl, (_ctx))
 
 void aem_gc_run(struct aem_gc_context *ctx);
 
 void aem_gc_mark(struct aem_gc_object *obj, struct aem_gc_context *ctx);
+#define AEM_GC_MARK_OBJ(_obj) do { \
+	aem_typeof(_obj) _obj_ = (_obj); \
+	if (_obj_) aem_gc_mark(&_obj_->gc, ctx); \
+} while (0)
+
+#define AEM_GC_FREE_DECL(_tp, _obj) void _tp##_free(struct aem_gc_object *obj, struct aem_gc_context *ctx)
+#define AEM_GC_DTOR_DECL(_tp, _obj) void _tp##_dtor(struct aem_gc_object *obj, struct aem_gc_context *ctx)
+#define AEM_GC_MARK_DECL(_tp, _obj) void _tp##_mark(struct aem_gc_object *obj, struct aem_gc_context *ctx)
+
+#define AEM_GC_GET_OBJ(_tp, _obj) struct _tp *_obj = caa_container_of(obj, struct _tp, gc);
+
+#define AEM_GC_VTBL_INST(_tp)     \
+struct aem_gc_vtbl _tp##_vtbl = { \
+	.name = #_tp,             \
+	.free = _tp##_free,       \
+	.dtor = _tp##_dtor,       \
+	.mark = _tp##_mark,       \
+};
 
 
 void aem_gc_ref(struct aem_gc_object *obj);
